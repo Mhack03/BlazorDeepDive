d98682aae005eb09b75b93e0ca4e715690484127
Mark Anthony Mordido <mark.mordido@gmail.com>
2024-08-26 00:21:37 +0800

    Add project files.

---
+++ ServerManagement/Models/Server.cs
@@ -0,0 +1,17 @@
+﻿namespace ServerManagement.Models
+{
+    public class Server
+    {
+        public Server()
+        {
+            Random random = new Random();
+            int randomeNumber = random.Next(0, 1);
+            IsOnline = randomeNumber == 0? false : true;
+        }
+        public int ServerId { get; set; }
+        public bool IsOnline { get; set; }
+        public string? Name { get; set; }
+        public string? City { get; set; }
+
+    }
+}
