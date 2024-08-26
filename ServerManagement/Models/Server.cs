243e3c80361f38c02b9e068ab71ddff69529e5f0
Mark Anthony Mordido <mark.mordido@gmail.com>
2024-08-27 01:44:25 +0800

    Output City button list

--- ServerManagement/Models/Server.cs
+++ ServerManagement/Models/Server.cs
@@ -5,13 +5,12 @@
         public Server()
         {
             Random random = new Random();
-            int randomeNumber = random.Next(0, 1);
+            int randomeNumber = random.Next(0, 2);
             IsOnline = randomeNumber == 0? false : true;
         }
         public int ServerId { get; set; }
         public bool IsOnline { get; set; }
         public string? Name { get; set; }
         public string? City { get; set; }
-
     }
 }
